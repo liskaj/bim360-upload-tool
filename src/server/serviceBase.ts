7fd39db10735b67af4bd802c055a9cc35a192ee8
Jan Liska <jan.liska@autodesk.com>
2020-02-27 21:21:03 +0100

    added wrappers for PUT, POST

--- src/server/serviceBase.ts
+++ src/server/serviceBase.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import * as fs from 'fs';
 
+import axios from 'axios';
+
 export class ServiceBase {
     private _router: express.Router;
 
@@ -18,6 +20,55 @@ export class ServiceBase {
     }
 
     protected initializeRoutes(): void {
+    }
+
+    protected post(url: string, token: string, inputs?: any, params?: any, additionalHeaders?: { [key: string]: string }): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            let headers = {
+                'Authorization': `Bearer ${token}`,
+            };
+
+            if (additionalHeaders) {
+                headers = Object.assign(headers, additionalHeaders);
+            } else  {
+                headers['Accept'] = 'application/json';
+                headers['Content-Type'] = 'application/json';
+            }
+            axios({
+                method: 'post',
+                url: url,
+                headers: headers,
+                data: inputs,
+                params: params
+            }).then((res) => {
+                resolve(res.data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
+
+    protected put(url: string, token: string, inputs?: any, params?: any, additionalHeaders?: { [key: string]: string }): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            let headers = {
+                'Authorization': `Bearer ${token}`
+            };
+            if (additionalHeaders) {
+                headers = Object.assign(headers, additionalHeaders);
+            }
+
+            axios({
+                method: 'put',
+                url: url,
+                headers: headers,
+                data: inputs,
+                params: params
+            }).then((res) => {
+                resolve(res.data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
     }
 
     protected readData(fileName: string): Promise<any> {
